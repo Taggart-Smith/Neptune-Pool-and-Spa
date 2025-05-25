38f5a5d9deb2a994e5f0fe292799444db1a662f9
Taggart Smith <taggartsmith@macbookpro.lan>
2025-05-25 16:45:10 -0600

    Updated home page

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
       <div className="text-center">
         <h1 className="text-4xl font-bold">Neptune Pool & Spa</h1>
         <p className="text-lg">Your trusted pool cleaning service.</p>
+      <h1 className="text-4xl font-bold">Contact Us</h1>
       </div>
 
       <div className="flex flex-col items-center">
