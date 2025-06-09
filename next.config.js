ce68bba536d56f312f8786497a0c207c5eac6a8d
Taggart Smith <smithtaggart15@gmail.com>
2025-06-09 14:54:42 -0600

    Updated Config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   basePath: '/Neptune-Pool-and-Spa',
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
