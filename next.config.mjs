3985ad4ed9fa92bee31fe16853889589a056df4a
Taggart Smith <smithtaggart15@gmail.com>
2025-06-09 14:08:26 -0600

    Adjusted code to be compatable with github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Optional: Set basePath if your repo is not your GitHub username (e.g., /neptune-app)
+  basePath: '/neptune-app',
+  trailingSlash: true,
+};
 
 export default nextConfig;
